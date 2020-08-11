408365ec953a5d1890c25562803cc47d15c0789a
Soph <andyrivest@gmail.com>
2020-08-11 15:52:29 +0300

    haHa

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -51,6 +51,7 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
 fun main() {
     val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
     println("Root product: $x1x2")
+    print("sofa")
 }
 
 /**
